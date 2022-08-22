01a958d54972b00705279846c3ae1276b26d3dfc
Braian Jassan <braianjassan@gmail.com>
2022-08-22 17:14:12 -0300

    add sale routes

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const connectDB = require('./config/db');
 
 // Routes
 const userRoutes = require('./routes/userRoutes');
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
+const saleRoutes = require('./routes/saleRoutes');
+
 const cors = require('cors');
+// const { use } = require('./routes/userRoutes');
 
 connectDB();
 dotenv.config();
 
 const app = express();
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 app.use(express.json({ limit: '10kb' }));
 
@@ -19,6 +22,7 @@ app.use(cors());
 
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/sale', saleRoutes);
 
 const port = process.env.PORT || 4500;
 
